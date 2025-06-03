ff38684c223690c71e53a8404e49b20688834dac
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-03 16:27:23 +0530

    Implemented SubjectName with web3forms.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import ContactDemo from "./components/ContactDemo";
 import Demo from "./components/Demo";
+import SubjectName from "./components/SubjectName";
 
 function App() {
   return (
     <>
       <h1 className="text-4xl text-center my-5">Web3Forms</h1>
-      <Demo />
-      <ContactDemo />
+      {/* <Demo /> */}
+      {/* <ContactDemo /> */}
+      <SubjectName />
+      
     </>
   );
 }
